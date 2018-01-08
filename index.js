8bd380768e028a10aaff6e99c70b9959e9a17de0
Liam Martens <hi@liammartens.com>
2018-01-08 21:54:38 +0100

    Fixed . replace

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ class BrunchInjectPlugin {
         while((matches=rx.exec(data))!=undefined) {
             if(matches && matches.length>1) {
                 const inject_file = matches[1].replace(/^'/, '').replace(/'$/, '');
-                const extension = path.extname(inject_file).replace(/^.+/, '');
+                const extension = path.extname(inject_file).replace(/^\.+/, '');
                 const fullpath = path.join(path.dirname(file), inject_file);
                 const inject_data = this.config.parse(fs.readFileSync(fullpath).toString().replace(/'/, '\\\'').split(/\r|\n/).join('\'+\''), inject_file, extension);
                 data = data.replace(rx, '\''+inject_data+'\'');
