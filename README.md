1d2ae63e2ae5cd77db96103d2547c521382cdece
Liam Martens <hi@liammartens.com>
2018-01-08 21:42:52 +0100

    brunch inject package

---
+++ README.md
@@ -0,0 +1,29 @@
+# Brunch Inject
+Brunch inject is a small plugin for injecting file contents into JS files as strings and parse them if needed.
+
+## Installation
+Install using npm  
+`npm install inject-brunch`
+
+## Brunch config
+Add plugin to brunch config  
+```
+module.exports.plugins = {
+    inject: {
+        // customize the inject function name
+        fn: 'inject',
+        parse: function(data) {
+            // here you can customize the data parser
+            // can be omitted
+            return yourparser(data);
+        }
+    }
+}
+```
+
+## Usage
+Using the default config you will have something like this
+```
+// injected data will be wrapped in single quotes
+const data = inject('file');
+```
---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+class BrunchInjectPlugin {
+    constructor(config) {
+        this.config = config.plugins.inject || {};
+        if(!this.config.fn) this.config.fn = 'inject';
+        if(!this.config.parse) this.config.parse = data => data;
+    }
+
+    compile({data, path: file}) {
+        const rx = new RegExp('\\s+'+this.config.fn+'\\((.+?)\\)', 'm');
+        let matches;
+        while((matches=rx.exec(data))!=undefined) {
+            if(matches && matches.length>1) {
+                const inject_file = matches[1].replace(/^'/, '').replace(/'$/, '');
+                const fullpath = path.join(path.dirname(file), inject_file);
+                const inject_data = this.config.parse(fs.readFileSync(fullpath).toString().replace(/'/, '\\\'').split(/\r|\n/).join('\'+\''));
+                data = data.replace(rx, '\''+inject_data+'\'');
+            }
+        }
+        return Promise.resolve({
+            data: data
+        });
+    }
+}
+
+BrunchInjectPlugin.prototype.brunchPlugin = true;
+BrunchInjectPlugin.prototype.type = 'javascript';
+BrunchInjectPlugin.prototype.extension = 'js';
+
+module.exports = BrunchInjectPlugin;
